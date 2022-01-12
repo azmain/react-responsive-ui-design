e16e03ec70d17bfaf338aad82229bda4c62f723b
unknown <azmainnishan@gmail.com>
2022-01-12 13:07:44 +0600

    file name spelling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
-import './app.css';
+import './App.css';
 
 const App = () => {
     return (
